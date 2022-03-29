c222f412bb1b5603ce06cae810761b366d000245
Jamesalehandro <jamesalehandro@gmail.com>
2022-03-29 15:43:04 +0100

    Deployed Website

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const App = () => {
       <Router>
         <NavBar />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/officialwebsite" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/project" element={<Projects />} />
